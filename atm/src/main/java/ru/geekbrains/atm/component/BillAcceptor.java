766c6bf0ca9fc5c36c1ca55d8e5d35c18c37fb48
3dartix <info@3dartix.ru>
2020-09-06 23:20:33 +0200

    dz4. ATM application

---
+++ atm/src/main/java/ru/geekbrains/atm/component/BillAcceptor.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.atm.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class BillAcceptor {
+    public void open(){
+        //открыть купюроприемник
+    }
+
+    public void close(){
+        //закрыть купюроприемник
+    }
+}
